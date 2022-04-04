77d4db68e938e3862edd51dd064fe97d82063619
MaximBiryukov <biryukov,.maxim.vladimirovich@yandex.ru>
2022-04-04 12:11:19 +0400

    create factory

--- programs/staking-factory/src/lib.rs
+++ programs/staking-factory/src/lib.rs
@@ -1,15 +1,37 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+mod stacking_params;
+
+declare_id!("DBa1q9iY3ZrvXBgEpVq453adWqZUrVDmRQztiW6FRJek");
 
 #[program]
 pub mod staking_factory {
     use super::*;
+    // use anchor_spl::token;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, fee_percent: u8) -> Result<()> {
+        ctx.accounts.creator_pda.fee_percent = fee_percent;
+        ctx.accounts.creator_pda.authority = ctx.accounts.factory_creator.key();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(fee_percent: u8)]
+pub struct Initialize<'info> {
+    #[account(init,
+    payer = factory_creator,
+    space = 8 + 32 + 1,
+    seeds= [b"factory_creator"], bump
+    )]
+    creator_pda: Account<'info, FactoryCreator>,
+    #[account(mut)]
+    factory_creator: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct FactoryCreator {
+    pub authority: Pubkey,
+    pub fee_percent: u8,
+}
--- tests/staking-factory.ts
+++ tests/staking-factory.ts
@@ -1,16 +1,31 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program,web3} from "@project-serum/anchor";
 import { StakingFactory } from "../target/types/staking_factory";
+// import {SystemProgram, Keypair} from "@solana/web3.js"
 
 describe("staking-factory", () => {
+  const provider = anchor.Provider.env();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-
+  anchor.setProvider(provider);
   const program = anchor.workspace.StakingFactory as Program<StakingFactory>;
 
+  let factoryCreator = web3.Keypair.generate();
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+    await provider.connection.requestAirdrop(factoryCreator.publicKey,  anchor.web3.LAMPORTS_PER_SOL);
+    const [factoryCreatorPda, _] = await anchor.web3.PublicKey.findProgramAddress(
+        // @ts-ignore
+        ['factory_creator'],
+        program.programId
+    );
+    const tx = await program.rpc.initialize(3,{
+      accounts: {
+        factoryCreator: factoryCreator.publicKey,
+        creatorPda: factoryCreatorPda,
+        systemProgram: web3.SystemProgram.programId,
+      },
+      signers: [factoryCreator],
+
+    });
     console.log("Your transaction signature", tx);
   });
 });
